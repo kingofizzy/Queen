677759178a47b47ab4677af46725b2294c471de2
Hyper Network <123313247+kingofizzy@users.noreply.github.com>
2025-02-06 16:00:45 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -9,19 +9,19 @@ API_ID = "25742938"
 # -------------------------------------------------------------
 API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "7551118575:AAHAHM8vtLD0FD2Zeld5feLXxCDRgGoSUps")
 STRING1 = getenv("STRING_SESSION", None)
 DB_NAME = "shizuDB"
-MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7009601543"))
-BOT_ID = int(getenv("BOT_ID", "8113054986"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://architect2002:architect2002@cluster0.ccinu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(getenv("OWNER_ID", "7882531710"))
+BOT_ID = int(getenv("BOT_ID", "7551118575"))
 SUPPORT_GRP = "PBX_CHAT"
 UPDATE_CHNL = "HEROKUBIN_01"
-OWNER_USERNAME = "ll_BAD_MUNDA_ll"
+OWNER_USERNAME = "Itz_me_Makkaru"
 TIME_ZONE = "Asia/Kolkata"
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002056907061"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002472747523"))
 # --------------------------------------------------------------
-SUDOERS = list(map(int, getenv("SUDOERS", "7009601543").split()))
+SUDOERS = list(map(int, getenv("SUDOERS", "7882531710").split()))
 # --------------------------------------------------------------
 
 ### DONT TOUCH or EDIT codes after this line
